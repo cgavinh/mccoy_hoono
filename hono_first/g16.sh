c4f5bc0a5dc426c3f19f76a84b99e343d0bd5ea5
GavinHannerC <cgavinh@uw.edu>
2022-01-30 15:37:10 -0800

    first big job of 0.005 perturbations

--- hono_first/g16.sh
+++ hono_first/g16.sh
@@ -19,31 +19,10 @@ set
 echo "**********************************************" 
 
 # run Gaussian
-g16 hono_r1_m_005.gjf
-g16 hono_r1_p_005.gjf
-g16 hono_r2_m_005.gjf
-g16 hono_r2_p_005.gjf
+#g16 hono_r1_pert.gjf
+#g16 hono_eq.gjf
 g16 hono_r3_m_005.gjf
 g16 hono_r3_p_005.gjf
-g16 hono_a1_m_5.gjf
-g16 hono_a1_p_5.gjf
-g16 hono_a2_m_5.gjf
-g16 hono_a2_p_5.gjf
-g16 hono_d1_m_5.gjf
-g16 hono_d1_p_5.gjf
-formchk hono_r1_m_005.chk
-formchk hono_r1_p_005.chk
-formchk hono_r2_m_005.chk
-formchk hono_r2_p_005.chk
-formchk hono_r3_m_005.chk
-formchk hono_r3_p_005.chk
-formchk hono_a1_m_5.chk
-formchk hono_a1_p_5.chk
-formchk hono_a2_m_5.chk
-formchk hono_a2_p_5.chk
-formchk hono_d1_m_5.chk
-formchk hono_d1_p_5.chk 
-
 
 
 
