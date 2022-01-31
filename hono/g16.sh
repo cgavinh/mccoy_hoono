e702f84f540715f6b47946fb873c7c209b94ddb2
GavinHannerC <cgavinh@uw.edu>
2022-01-31 11:54:25 -0800

    bigger perturbations

--- hono/g16.sh
+++ hono/g16.sh
@@ -19,30 +19,30 @@ set
 echo "**********************************************" 
 
 # run Gaussian
-g16 hono_r1_m_01.gjf
-g16 hono_r1_p_01.gjf
-g16 hono_r2_m_01.gjf
-g16 hono_r2_p_01.gjf
-g16 hono_r3_m_01.gjf
-g16 hono_r3_p_01.gjf
-g16 hono_a1_m_10.gjf
-g16 hono_a1_p_10.gjf
-g16 hono_a2_m_10.gjf
-g16 hono_a2_p_10.gjf
-g16 hono_d1_m_10.gjf
-g16 hono_d1_p_10.gjf
-formchk hono_r1_m_01.chk
-formchk hono_r1_p_01.chk
-formchk hono_r2_m_01.chk
-formchk hono_r2_p_01.chk
-formchk hono_r3_m_01.chk
-formchk hono_r3_p_01.chk
-formchk hono_a1_m_10.chk
-formchk hono_a1_p_10.chk
-formchk hono_a2_m_10.chk
-formchk hono_a2_p_10.chk
-formchk hono_d1_m_10.chk
-formchk hono_d1_p_10.chk 
+g16 hono_r1_m_02.gjf
+g16 hono_r1_p_02.gjf
+g16 hono_r2_m_02.gjf
+g16 hono_r2_p_02.gjf
+g16 hono_r3_m_02.gjf
+g16 hono_r3_p_02.gjf
+g16 hono_a1_m_20.gjf
+g16 hono_a1_p_20.gjf
+g16 hono_a2_m_20.gjf
+g16 hono_a2_p_20.gjf
+g16 hono_d1_m_20.gjf
+g16 hono_d1_p_20.gjf
+formchk hono_r1_m_02.chk
+formchk hono_r1_p_02.chk
+formchk hono_r2_m_02.chk
+formchk hono_r2_p_02.chk
+formchk hono_r3_m_02.chk
+formchk hono_r3_p_02.chk
+formchk hono_a1_m_20.chk
+formchk hono_a1_p_20.chk
+formchk hono_a2_m_20.chk
+formchk hono_a2_p_20.chk
+formchk hono_d1_m_20.chk
+formchk hono_d1_p_20.chk 
 
 
 
